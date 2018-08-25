16b602a5635e110cef359f8dea3823a082cd78ab
marcio sansone <msansone@gmail.com>
2018-08-25 12:54:43 +0100

    first commit

--- index.js
+++ index.js
@@ -6,7 +6,7 @@
 // Dependency
 var http = require('http');
 var url = require('url');
-
+var StringDecoder = require('string_decoder').StringDecoder;
 
 // The server shoukd respond to all request with string
 var server = http.createServer(function(req,res){
@@ -20,13 +20,35 @@ var server = http.createServer(function(req,res){
     // Get the HTTP request
     var method = req.method.toLowerCase();
 
+    // Get the query string as an object
+    var queryString = parseUrl.query;
+
+    //Get the headers of the object
+    var headers = req.headers
+
+    // Get the payload
+    var decoder = new StringDecoder('utf-8');
+    var buffer = '';
+    req.on('data',function(data){
+        buffer+= decoder.write(data);
+    })
+    req.on('end', function(){
+        buffer+=decoder.end();
+
+        // Send the response
+        res.end('Hello word\n');
+
+        //Log the request path
+        console.log('Request receved on path: ' + trimPath + 
+        ' with this method ' + method);
+        console.log('queryString: ', queryString);
+        console.log('header: ',headers);
+        console.log('body: ',buffer);
+
+    })
 
 
-    // Send the response
-    res.end('Hello word');
-
-    //Log the request path
-    console.log('Request receved on path: ' + trimPath + ' with this method ' + method);
+    
     
 })
 
