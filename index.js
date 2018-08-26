683b96809344d89c73c2412088117cae5855d530
marcio sansone <msansone@gmail.com>
2018-08-26 12:24:36 +0100

    first funcional release

--- index.js
+++ index.js
@@ -35,26 +35,62 @@ var server = http.createServer(function(req,res){
     req.on('end', function(){
         buffer+=decoder.end();
 
-        // Send the response
-        res.end('Hello word\n');
+        // Choose the handler this should go to If one is not found, use notFound handler
+        var choseHandler = typeof( router[trimPath]) !== 'undefined' ? router[trimPath] : handlers.notFound;
 
-        //Log the request path
-        console.log('Request receved on path: ' + trimPath + 
-        ' with this method ' + method);
-        console.log('queryString: ', queryString);
-        console.log('header: ',headers);
-        console.log('body: ',buffer);
+        //Construct the data object to send the handler
+        var data = {
+            'trimedPath':trimPath,
+            'queryStringObject': queryString,
+            'method': method,
+            'handlers' : handlers,
+            'payload': buffer
+        };
 
-    })
+        // Route to request specified in the router
+        choseHandler(data, function(statusCode, payload){
+            // Use the status code called back by the handler, or default 
+            status= typeof(statusCode) == 'number' ? statusCode : 200;
 
+            // Use the payload called the back by handler,  or default to empty object
+            payload = typeof(payload) == 'object' ? payload : {};
 
-    
-    
+            // Convert the payload to a string 
+            var payloadString = JSON.stringify(payload);
+
+            // Return de response
+            res.writeHead(statusCode);
+            res.end(payloadString);
+
+            //Log the request path
+            console.log('Returning this response' , statusCode,  payloadString);
+
+        })
+
+    })    
 })
 
-
 // Start the server and have ir listen in port 3000
-
 server.listen(3000,function(){
     console.log('Console is linten in port 3000 now.');
 })
+
+
+//Define a request router
+var handlers= {};
+
+// Sample handler
+handlers.sample = function(data, callback){
+    // Callback a http status code, and a payload object
+    callback(406,{'name': 'sample handler'})
+};
+
+// Not found handler
+handlers.notFound = (data, callback) => {
+    callback(404)
+};
+
+// Define a request router
+var router = {
+    'sample':handlers.sample
+};
